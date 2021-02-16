43292829890925dc6a6d86bbfa93c20ed32a19ac
ROLAND KIRALY <43180177+toobig4u@users.noreply.github.com>
2021-02-16 14:49:18 +0100

    broken project into smaller components / added styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from "./logo.svg";
 import "./App.css";
-import HomePage from "./components/homepage.component";
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
